21a7b65ff1fc468da883be07c5c2a79b80179032
Jerrod Long <me@jerrodlong.com>
2017-07-24 05:59:44 -0400

    [add] Basic profile page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,6 @@
 @extends('layout')
 
 @section('content')
-<h1>Profile goes here</h1>
+<h1 class="title is-1">Welcome to the profile of {{ auth()->user()->name }}</h1>
+<p>More to come in this space &hellip;</p>
 @endsection
