44b1059640b82565dcd2f5dded83134181a7c845
Jerrod Long <jlong@doeanderson.com>
2017-07-31 02:13:11 -0400

    [update] Reorder snippet table migration to add foriegn key.

---
+++ database/migrations/2017_07_31_060830_create_snippets_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSnippetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('snippets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('body');
+            $table->integer('user_id')->unsigned();
+            $table->integer('forked_id')->nullable()->unsigned();
+            $table->foreign('forked_id')->references('id')->on('snippets');
+            $table->integer('language_id')->nullable()->unsigned();
+            $table->foreign('language_id')->references('id')->on('languages');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('snippets');
+    }
+}
